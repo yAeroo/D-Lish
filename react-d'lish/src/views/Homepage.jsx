9be510d9856e69e6581c89ed19bea4e8765f0d68
FloppaCoder <87096987+FloppaCoder@users.noreply.github.com>
2023-04-10 22:16:50 -0600

    Homepage Inicial

---
+++ react-d'lish/src/views/Homepage.jsx
@@ -0,0 +1,18 @@
+import logo from './logo.svg';
+import './App.css';
+import Navbar from './components/Navbar';
+import NavLinks from './components/NavLinks';
+import Headers from './components/Header';
+import Hero from './components/Hero';
+
+function App() {
+  return (
+
+    <div>
+      <Headers />
+    </div>
+
+  );
+}
+
+export default App;
