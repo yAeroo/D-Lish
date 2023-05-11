90551be45ed3eba29c02248079f41232b1e3f8e2
Racplus <oscararcepineda@gmail.com>
2023-05-11 03:21:48 -0600

    hola
    pequeño commit de algo que se me olvido

--- react-d'lish/src/views/Index.jsx
+++ react-d'lish/src/views/Index.jsx
@@ -53,6 +53,8 @@ export default function Index() {
 
                     <div className="hidden lg:flex lg:gap-x-12">
                        
+                    {/* ----------------Ete search ------------------*/}
+
                     <form class="flex items-center">   
                         <label for="simple-search" class="sr-only">Search</label>
                         <div class="relative w-full">
@@ -221,33 +223,22 @@ export default function Index() {
                         <div className="mt-6 flow-root">
                             <div className="-my-6 divide-y divide-gray-500/10">
                                 <div className="space-y-2 py-6">
-                                    <Link
-                                        to="#"
-                                        className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"
-                                    >
-                                        Inicio
-                                    </Link>
+                                      {/* ----------------Ete search Mobile------------------*/}
+                    
+                                <form class="flex items-center">   
+                                    <label for="simple-search" class="sr-only">Search</label>
+                                    <div class="relative w-full">
+                                        <div class="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
+                                            <svg aria-hidden="true" class="w-5 h-5 text-gray-500 dark:text-gray-400" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z" clip-rule="evenodd"></path></svg>
+                                        </div>
+                                        <input type="text" id="simple-search" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-orange-500 focus:border-orange-500 block w-full pl-10 p-2.5 " placeholder="Search" required></input>
+                                    </div>
+                                    <button type="submit" class="p-2.5 ml-2 text-sm font-medium text-gray-900 bg-orange-700 rounded-lg border border-orange-700 hover:bg-orange-800 focus:ring-4 focus:outline-none focus:ring-orange-300 ">
+                                        <svg class="w-5 h-5 text-white" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"></path></svg>
+                                        <span class="sr-only">Search</span>
+                                    </button>
+                                </form>
 
-                                    <Link
-                                        to="#"
-                                        className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"
-                                    >
-                                        Direccion 2
-                                    </Link>
-
-                                    <Link
-                                        to="#"
-                                        className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"
-                                    >
-                                        Direccion 3
-                                    </Link>
-
-                                    <Link
-                                        to="#"
-                                        className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"
-                                    >
-                                        Direccion 4
-                                    </Link>
                                 </div>
                                 <div className="py-6">
                                     <div className="flex items-center">
@@ -272,21 +263,21 @@ export default function Index() {
                                             to="#"
                                             className="block rounded-md  py-2 text-base font-medium text-gray-900 hover:bg-gray-700 hover:text-white"
                                         >
-                                            Your Profile
+                                            Mi perfil
                                         </Link>
 
                                         <Link
                                             to="#"
                                             className="block rounded-md py-2 text-base font-medium text-gray-900 hover:bg-gray-700 hover:text-white"
                                         >
-                                            Settings
+                                            Opciones
                                         </Link>
 
                                         <Link
                                             to="#"
                                             className="block rounded-md  py-2 text-base font-medium text-gray-900 hover:bg-gray-700 hover:text-white"
                                         >
-                                            Sign out
+                                           Cerrar sesion
                                         </Link>
 
                                         <hr></hr>
