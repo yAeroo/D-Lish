f727ef58ce5e1927f2629e6b3a44b06b3d8658be
FloppaCoder <87096987+FloppaCoder@users.noreply.github.com>
2023-05-13 11:29:18 -0600

    Cambio menor footer

--- react-d'lish/src/components/Footer.jsx
+++ react-d'lish/src/components/Footer.jsx
@@ -21,7 +21,7 @@ const Footer = () => {
 
                
             </div>
-            <hr class="my-6 border-gray-200 sm:mx-auto dark:border-gray-700 lg:my-8" />
+            <hr class="my-6 border-gray-200 sm:mx-auto dark:border-white-700 lg:my-8" />
             <div class="sm:flex sm:items-center sm:justify-between">
                 <span class="text-sm text-gray-500 sm:text-center dark:text-gray-400">
                     © 2023{" "}
