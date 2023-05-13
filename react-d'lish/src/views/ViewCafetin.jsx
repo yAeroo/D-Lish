16733e53b4f8cca8cd77aeeafae2910d33fb4cc2
FloppaCoder <87096987+FloppaCoder@users.noreply.github.com>
2023-05-13 01:50:39 -0600

    Implementando Iconos de React en Vista de Cafetin

--- react-d'lish/src/views/ViewCafetin.jsx
+++ react-d'lish/src/views/ViewCafetin.jsx
@@ -1,9 +1,9 @@
-import React from 'react'
-import User from "./img/user.png";
-import Back from "./img/back.png";
-import Like from "./img/like.png";
+import React from 'react';
 import Pollo from "./img/pollo.jpg";
 import Carne from "./img/carne.jpg";
+import { IoChevronBack } from "react-icons/io";
+import { FaUserCircle } from "react-icons/io";
+import { FaRegHeart } from "react-icons/io";
 
 export default function ViewCafetin() {
   return (
@@ -14,13 +14,13 @@ export default function ViewCafetin() {
         
         <section className="bg-white py-6 px-4">
           <div className="flex items-center bg-white">
-            <img src={Back} alt="" className="w-10 h-12 mr-4" />
-            <img src={User} alt="" className="w-12 h-12 ml-auto" />
+            <IoChevronBack className="w-10 h-12 mr-4" />
+            <FaUserCircle className="w-12 h-12 ml-auto" />
           </div>
         
           <div className='flex items-center'>
             <h2 className="text-7xl font-bold mt-10 px-2">Miguel Magone</h2>
-            <img src={Like} alt="" className="w-8 h-8 ml-3 mt-16" />
+            <FaRegHeart className="w-8 h-8 ml-3 mt-16" />
           </div>
         
           
@@ -29,7 +29,7 @@ export default function ViewCafetin() {
         <section className="bg-secu text-white mt-6 min-h-screen rounded-t-xl items-center justify-between">
           <div className="flex items-center justify-between">
             <h2 className="text-2xl font-bold px-2 ml-6 mt-6 text-terc">● Abierto</h2>
-            <img src={Like} alt="" className="w-8 h-8 mr-8 mt-7 " />
+            <FaRegHeart className="w-8 h-8 mr-8 mt-7 " />
           </div>
         
           <section className="flex items-center justify-between text-white my-8 px-6">
