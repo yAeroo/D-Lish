2b95bf472254ed36ea6f32cf300a4a9300650d5c
FloppaCoder <87096987+FloppaCoder@users.noreply.github.com>
2023-07-01 00:04:10 -0600

    Homepage | Integracion de DaisyUI

--- react-d'lish/src/components/Header.jsx
+++ react-d'lish/src/components/Header.jsx
@@ -8,27 +8,24 @@ import ButtonOutline from './ButtonOutline';
 const Header = () => {
     return (
         <>
-            <section className='bg-gradient-to-r  bg-Hero bg-cover bg-center h-screen py-4 md:px-24 px-4'>
-                <Navbar />
-
-                <div className='flex md:flex-row flex-col gap-5 pt-20'>
-                    <div className='flex-1'>
-                        <h1 className='text-base md:text-5xl text-4xl font-semibold tracking-wide md:leading-tight 
-                    leading-snug'>Ordena tu comida de manera rápida y sencilla.</h1>
-                        <p className='text-terc font-semibold md:w-2/3 md:py-4 py-2 leading-relaxed'>
-                            ¿Cansado de las largas filas de espera? Ordena y recoge tu comida con nosotros.
-                        </p>
-                        <br />
-                        <div className='flex md:gap-4 gap-2 flex-wrap'>
-                            <ButtonFill>Registrate</ButtonFill>
-                            <ButtonOutline />
-                        </div>
-                    </div>
-                    <div className='flex-1 flex justify-center py-1'>
-                        <img src={HeroImg} alt="hero" className='h-3/5' />
+            <section className='px-4 md:px-24 py-4'>
+                <Navbar></Navbar>
+            </section>
+            
+                <div className="hero min-h-screen animate-fade-up animate-duration-400 animate-ease-in-out">
+                <div className="hero-content flex-col lg:flex-row-reverse">
+                    <img src={HeroImg} className="w-[20rem] rounded-lg shadow-2xl" />
+                    <div>
+                    <h1 className="text-5xl font-bold text-primary">Ordena tu comida de manera rapida y sencilla</h1>
+                    <p className="py-6">Cansado de filas de espera? Ingresa y ordena tu comida con nosotros.</p>
+                    
+                    <button className="btn btn-error mr-4">Registrate</button>
+                    <button className="btn btn-outline btn-error">Iniciar Sesión</button>
                     </div>
                 </div>
-            </section>
+            </div>
+            
+            
         </>
     );
 };
