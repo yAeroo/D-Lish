37e7298ccd97b852b68323877126abc097ad5c2f
Vinic`-` <viictorgarcia01@gmail.com>
2023-07-01 00:11:17 -0600

    prueba de dish.jsx

--- react-d'lish/src/views/Dish.jsx
+++ react-d'lish/src/views/Dish.jsx
@@ -2,12 +2,17 @@ import React from 'react'
 // Habilitando archivo para router link
 import { Link } from "react-router-dom";
 
+
 const Dish = () => {
-  return (
-    <div>
-      Pues Este es el DLISH XDD
+return (
+    <div>    
+        <div className='grid place-items-center'>
+            <h1 className='bg-primary rounded-md animate-fade'>When te dicen quien hara el DISH</h1>
+            <h2 className='bg-secondary rounded-md animate-fade-down'>Soy Vinic</h2>
+        </div>
+        
     </div>
-  )
+    )
 }
 
 export default Dish
