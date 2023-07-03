b084bf3db84e80c8d17e73182faf6c1dd8ecf36c
Vinic`-` <viictorgarcia01@gmail.com>
2023-07-02 19:44:12 -0600

    Primer Diseño de la pagina de orden de Platillos
    Primeras opciones de diseño para el "carrito"

--- react-d'lish/src/views/Order.jsx
+++ react-d'lish/src/views/Order.jsx
@@ -4,10 +4,12 @@ import { Link } from "react-router-dom";
 
 function Order() {
   return (
-    <div>
-        Oigan Pibes aca ira la pagina de Ordenes 
-        Link para ir a "Dish👴🏻"
-        <Link to="/dish"> Aca we</Link>
+    <div> 
+      <section className='px-4 md:px24 py-4'> 
+      <h1>Oigan Pibes aca ira la pagina de Ordenes 
+        Link para ir a "Dish👴🏻"<Link className='grid place-items-center' to="/dish"> Aca we</Link></h1>
+        </section>     
+
     </div>
   )
 }
