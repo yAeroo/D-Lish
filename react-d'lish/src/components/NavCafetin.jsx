76a52a6e789ecfb8ae7aa7124629ce8261a66eca
FloppaCoder <87096987+FloppaCoder@users.noreply.github.com>
2023-07-03 00:56:50 -0600

    Cafetin | Creacion y correccion de componente NavCafetin

--- react-d'lish/src/components/NavCafetin.jsx
+++ react-d'lish/src/components/NavCafetin.jsx
@@ -9,7 +9,7 @@ const NavCafetin = () => {
         <nav className='flex justify-between rounded-2xl drop-shadow-md px-9 py-5 list-none '>
             
             <div className='flex items-center'><IoChevronBack className="w-10 h-12 mr-5 font-bold"/> 
-            <h1 className="title text-2xl hidden md:block">Regresar</h1>
+            <h1 className="title text-[1.5rem] hidden md:block">Regresar</h1>
             <h1 className="title hidden">Regresar</h1> </div>
             
             <FaUserCircle className="w-12 h-12 ml-auto"/>
