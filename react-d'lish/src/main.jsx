261e247bfefd5b3ae8e98a31660659e8103bae18
Mateo Elías <oscarmateoelias@gmail.com>
2023-07-10 00:12:25 -0600

    Router pendiente

--- react-d'lish/src/main.jsx
+++ react-d'lish/src/main.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import './index.css'
+// Router
 import { RouterProvider } from 'react-router-dom'
 import router from './router'
-import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
     <React.StrictMode>
-        <RouterProvider router={router} />  
+        <RouterProvider router={router} />
     </React.StrictMode>,
 )
