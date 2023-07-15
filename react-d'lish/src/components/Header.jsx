2bc40cd092d1db0ce864644eabfe192ef139767e
FloppaCoder <87096987+FloppaCoder@users.noreply.github.com>
2023-07-15 13:31:04 -0600

    Te odio perry el ornitoHERO :'v

--- react-d'lish/src/components/Header.jsx
+++ react-d'lish/src/components/Header.jsx
@@ -20,30 +20,35 @@ const Header = () => {
         </div>
       </section>
 
-      <div className="hero min-h-screen bg-gradient-to-r from-slate-900 to-base-100">
-        <div className="hero-content flex-col md:flex-row-reverse px-10">
-          <img
-            src={HeroImg}
-            className="w-[34rem] md:-mt-[8rem] md:-mr-[8rem] rounded-lg animate-fade-left animate-duration-600 animate-ease-in-out"
-          />
-          <div className="md:-mt-[16rem] md:-ml-[12rem] animate-fade-up animate-once animate-delay-[600ms] drop-shadow-md">
-            <h1 className="md:text-7xl text-5xl font-bold text-primary drop-shadow-lg">
-              Ordena tu comida de manera rapida y sencilla
-            </h1>
-            <p className="md:text-2xl py-6">
-              Cansado de filas de espera? Ingresa y ordena tu comida con
-              nosotros.
-            </p>
+      <div class="hero min-h-screen bg-gradient-to-r from-slate-900 to-base-100">
+  <div class="hero-content flex flex-col md:flex-row-reverse px-10 md:px-10 md:mt-[-6rem]">
+    <div class="md:w-2/5 rounded-lg mt-1 md:ml-1 animate-fade-left animate-duration-600 animate-ease-in-out order-1 md:order-1">
+      <img src={HeroImg} class="w-full 2xl:ml-[15rem]" />
+    </div>
+    
+    <div class="md:w-3/5 order-1 md:order-2 animate-fade-up animate-once animate-delay-[600ms] 2xl:ml-[-20rem]">
+      <h1 class="md:text-6xl text-4xl font-bold text-primary drop-shadow-lg md:mt-[-4rem] ">
+        Ordena tu comida de manera rápida y sencilla
+      </h1>
+      <p class="md:text-2xl py-4 md:py-6">
+        ¿Cansado de filas de espera? Ingresa y ordena tu comida con nosotros.
+      </p>
 
-            <button className="btn btn-error mr-4 drop-shadow-lg text-zinc-50">
-              Registrate
-            </button>
-            <button className="btn btn-outline btn-error drop-shadow-lg ">
-              Iniciar Sesión
-            </button>
-          </div>
-        </div>
+      <div class="flex space-x-4">
+        <button class="btn btn-error drop-shadow-lg text-zinc-50">
+          Regístrate
+        </button>
+        <button class="btn btn-outline btn-error drop-shadow-lg">
+          Iniciar Sesión
+        </button>
       </div>
+    </div>
+  </div>
+</div>
+
+
+
+
 
       <div className="flex items-center justify-center mt-[10rem] mx-[2rem] md:mx-[1rem] lg:mx-[15rem]">
         <h1 className="md:text-7xl text-6xl font-bold text-primary drop-shadow-lg text-center">
@@ -53,7 +58,7 @@ const Header = () => {
 
       <div className="flex flex-col items-center justify-center min-h-screen mt-[6rem] md:mx-[22rem]">
 
-        <div className="flex flex-col items-center justify-center w-full md:flex-col lg:flex-row md:mt-[6rem]">
+        <div className="flex flex-col 2xl:flex-row items-center justify-center w-full md:mt-[6rem]">
           <div className="w-96 bg-base-100 shadow-xl">
             <figure className="px-10 pt-10 bg-base-100 flex items-center justify-center ">
               <IoFastFoodOutline className="text-6xl" />
