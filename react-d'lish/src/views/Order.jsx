35c76bba2c3521c9079455ab2c0bde048b993dcb
Vinic`-` <viictorgarcia01@gmail.com>
2023-07-17 14:07:20 -0600

    css carpeta

--- react-d'lish/src/views/Order.jsx
+++ react-d'lish/src/views/Order.jsx
@@ -1,51 +1,36 @@
-import React from 'react'
+import from './css/cutoff-border.css';
 // Habilitando archivo para router link
 import { Link } from "react-router-dom";
+import Navbar from '../components/Navbar';
 
 function Order() {
   return (
-    <div className='flex items-center justify-center w-full animate-fade-down'> 
+    <>
+    {/* Apartado de NavBar */}
+          <section className='px-4 md:px-24 py-4 bg-gradient-to-r'>
+            <div className='animate-fade-up animate-once animate-delay-[1300ms]'><Navbar></Navbar></div>
+          </section>
+
+    {/* Cuerpo del trabajo + animación  */}
+  <div className='flex items-center justify-center w-full animate-fade-down animate-delay-[1300ms]'> 
+
+    {/* Estructura del cuadrito */}
  <div  className="h-screen grid grid-cols-1 md:grid-cols-3 tracking-wide">
-    <div id="pedidos" className="h-full col-start-2 py-20 place-self-center items-center md:  px-3">
-      <div id="image-dlish" className="">
-        <img src="../src/assets/logo/wide_orange.png" className="w-full rounded-t-lg"/>
+    <div className="h-full col-start-2 py-20 place-self-center items-center md:  px-3">
+      {/* Cuerpo del La card */}
+      <div id="card-body" className="grid grid-cols-1 justify-items-center px-20 py-10 bg-[#d6d3d1] rounded-b-lg  shadow-2xl">
+              <div id="order-title">
+                <h1 className="text-xl text-base-100 font-bold">Resumen del pedido</h1>
+              </div>
+              <div id="order-description" className="grid grid-cols-1">
+                <p className="text-center text-sm text-base-100 pt-2 pb-6 tracking-normal">Pedido Realizado en<br/>Cafeteria "Maria Auxiliadora"</p>
+              </div>
       </div>
-      <div id="card-body" className="grid grid-cols-1 justify-items-center px-20 py-10 bg-neutral rounded-b-lg  shadow-2xl">
-        <div id="order-title">
-          <h1 className="text-xl">Resumen del pedido</h1>
-        </div>
-
-        <div id="order-description" className="grid grid-cols-1">
-          <p className="text-center text-sm text-gray-600 pt-2 pb-6 tracking-normal">Pedido Realizado en<br/>Cafeteria "Maria Auxiliadora"</p>
-        </div>
-
-<div className=''>
-        <div id="order-item" className="grid grid-cols-3 rounded-lg mb-10">
-            <img src="\public\img\Almuerzos.png" alt="" srcset="" className="py-5 pl-5"/>
-          <div id="plan-pricing"className="py-5 pr-5">
-            <h2 className="md:text-sm px-5 tracking-normal text-xs">Almuerzo</h2>
-            <p className="md:text-sm px-5 text-gray-600 text-xs">$2.50</p>
-          </div>
-          <div id="change-link" className="grid place-content-center">
-          <h1><Link className='text-xs underline text-white font-medium' to="/dish"> Cambiar</Link></h1>
-          </div>
-        </div>
-</div>
-
-<br />
-
-        <div id="confirmation-bt">
-          <button className="rounded-lg mb-5 p-3 px-6 w-full text-white  bg-error shadow-2xl">Proceder a pagar</button>
-        </div>
-
-        <div id="cancel">
-        <button className="rounded-lg mb-5 p-3 px-6 w-full text-white  shadow-2xl">Cancelar</button>
-        </div>
-        
       </div>
-    </div>
   </div>
+
   </div>
+  </>
   )
 }
 
