3fcebe040677d86c8e32755c8a8e34c664218490
FloppaCoder <87096987+FloppaCoder@users.noreply.github.com>
2023-07-21 00:01:54 -0600

    Faltó este :v

--- react-d'lish/src/components/Navbar.jsx
+++ react-d'lish/src/components/Navbar.jsx
@@ -15,7 +15,7 @@ const Navbar = () => {
       {/* Eliminar "rounded-2xl" para lograr el Navbar cuadrado (y hacer los respectivos cambios en el componente que se requiera)  */}
       <nav className='flex justify-between rounded-2xl bg-neutral drop-shadow-md px-9 py-5 list-none '>
         {/* Link a Homepage */}
-        <Link to='/homepage'>
+        <Link to='/'>
           <img src="../src/assets/logo/icon_bw.png" alt="Logo" className='h-12 cursor-pointer my-auto' />
         </Link>
         <div className="md:flex hidden items-center gap-10 text-white md:my-0 my-7">
