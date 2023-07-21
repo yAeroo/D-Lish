46dee81afbb804f9d2d717abc372f234c95b2f65
FloppaCoder <87096987+FloppaCoder@users.noreply.github.com>
2023-07-20 18:41:00 -0600

    Cambios Homepage

--- react-d'lish/src/views/Homepage.jsx
+++ react-d'lish/src/views/Homepage.jsx
@@ -27,7 +27,7 @@ export default function Homepage() {
 
       <div className="hero min-h-screen bg-gradient-to-r from-slate-900 to-emerald-900">
 
-        <div className="md:mb-[-7rem] mb-[-8rem] custom-shape-divider-bottom-1689485827">
+        <div className="md:mb-[-7.5rem] mb-[-7.8rem] hidden md:block custom-shape-divider-bottom-1689485827">
           <svg data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1200 120" preserveAspectRatio="none">
             <path d="M321.39,56.44c58-10.79,114.16-30.13,172-41.86,82.39-16.72,168.19-17.73,250.45-.39C823.78,31,906.67,72,985.66,92.83c70.05,18.48,146.53,26.09,214.34,3V0H0V27.35A600.21,600.21,0,0,0,321.39,56.44Z" cla="shape-fill"></path>
           </svg>
