3b1d73038fdedd42233dee24d7d0ce9974e44d20
FloppaCoder <87096987+FloppaCoder@users.noreply.github.com>
2023-08-06 12:40:34 -0600

    Arreglos Menores | Nav Movil

--- react-d'lish/src/components/NavMobileIndex.jsx
+++ react-d'lish/src/components/NavMobileIndex.jsx
@@ -11,15 +11,15 @@ export default function NavMobileIndex() {
     <div className="btm-nav md:hidden text-primary z-10 bg-base-300">
       <Link to="../search" className="flex items-center gap-1">
         <FaSearch className="h-5 w-5 mx-auto" />
-        <span className="btm-nav-label">Busqueda</span>
+        <span className="btm-nav-label text-sm">Busqueda</span>
       </Link>
       <Link to="/" className="active bg-base-300">
         <FaHome className="h-5 w-5 mx-auto" />
-        <span className="btm-nav-label">Inicio</span>
+        <span className="btm-nav-label text-sm">Inicio</span>
       </Link>
       <Link to="../profile" className="flex items-center gap-1">
         <FaUserCircle className="h-5 w-5 ml-[0.1rem]" />
-        <span className="btm-nav-label">Perfil</span>
+        <span className="btm-nav-label text-sm">Perfil</span>
       </Link>
     </div>
   );
