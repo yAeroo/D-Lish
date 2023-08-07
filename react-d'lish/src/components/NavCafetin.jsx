aa0165e6d66e625ccafda0632f155adb64200c6e
Viiictor01 <87097376+Viiictor01@users.noreply.github.com>
2023-08-07 16:04:44 -0600

    nav marselo trash

--- react-d'lish/src/components/NavCafetin.jsx
+++ react-d'lish/src/components/NavCafetin.jsx
@@ -8,12 +8,12 @@ const NavCafetin = () => {
   return (
     <>
         <nav className='flex justify-between rounded-2xl drop-shadow-md px-9 py-5 list-none '>
-            
+        <Link onClick={() => window.history.back()}> 
             <div className='flex items-center'><IoChevronBack className="w-10 h-12 mr-5 font-bold"/>
-            <Link onClick={() => window.history.back()}>
+
             <h1 className="title text-[1.5rem] hidden md:block">Regresar</h1>
-            </Link>
             <h1 className="title hidden">Regresar</h1> </div>
+            </Link>
             
             <FaUserCircle className="w-12 h-12 ml-auto"/>
             
