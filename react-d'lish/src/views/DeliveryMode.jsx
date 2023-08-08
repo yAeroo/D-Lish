84743a4e4c5dac9f1c059e25bd75c31f773df500
Viiictor01 <87097376+Viiictor01@users.noreply.github.com>
2023-08-08 14:26:04 -0600

    Nueva Libreria React-Webcam + pequeños ajustes

--- react-d'lish/src/views/DeliveryMode.jsx
+++ react-d'lish/src/views/DeliveryMode.jsx
@@ -1,25 +1,29 @@
 import React from 'react'
+import NavCafetin from '../components/NavCafetin'
 
 
 function DeliveryMode() {
   return (
-    <div className='lg:ml-[7rem] my-[-65px] lg:my-[0px] h-[100%] profile-banner min-h-screen animate-fade animate-duration-500'>
+    <>
+    <NavCafetin></NavCafetin>
+    <div className=' profile-banner min-h-screen animate-fade animate-duration-500'>
 
         {/* Texto de arriba */}
-        <section className='text-center flex h-[inherit] w-full justify-center items-center flex-col'>
+        <section className='text-center flex  w-full justify-center  items-center flex-col'>
             <div className='p-3 pb-10 text-xl'>
-                <div className="px-6 py-5 font-semibold border-b text-secondary border-gray-100">Este es su QR</div>
+                <div className="px-6 py-5 font-bold text-3xl md:text-4xl lg:text-4xl xl:text-5xl border-b text-secondary border-gray-100">Este es su QR</div>
             </div>
 
           {/* Imagen  QR */}
-          <div className="  shadow-xl">
+          <div className=" ">
             <figure className=' ml-12 w-3/4 h-3/4  md:w-72 md:mr-11  md:h-72 '>
                 <img src="../src/assets/img/qrpreview.png" alt="QR" />
             </figure>
-          </div>
+          </div>  
         </section>
 
     </div>
+    </>
   )
 }
 
