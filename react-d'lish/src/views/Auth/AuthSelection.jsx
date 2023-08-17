6ea7f2e2d4a23413ce6d3363d49107bf32288f5c
MatMT <oscarmateoelias@gmail.com>
2023-08-17 09:25:44 -0600

    Marcelo huevon

--- react-d'lish/src/views/Auth/AuthSelection.jsx
+++ react-d'lish/src/views/Auth/AuthSelection.jsx
@@ -13,7 +13,7 @@ export default function AuthSelection() {
                     <img src={LogoImg} alt="LogoImg" className="w-auto h-32 self-center" />
                     <p className="text-white text-2xl font-title text-center font-bold my-5">Accede a D'Lish y disfruta de sabores únicos.</p>
 
-                    <div className="flex-col md:flex-row w-full flex justify-center mt-5 text-white">
+                    <div className="flex-col md:flex-row w-full flex justify-center items-center mt-5 text-white">
                         <div id="LoginCard">
                             <div className="card lg:card-side h-full !bg-[#2c2c2c] shadow-xl min-w-[300px] max-w-[300px] lg:min-w-[425px] lg:max-w-[425px]">
                                 <figure><img src={LoginCardImg} alt="LoginCardImg" className="w-full h-40 object-cover object-[100%_60%] lg:w-380 lg:h-[14.5rem]" /></figure>
@@ -21,19 +21,19 @@ export default function AuthSelection() {
                                     <h2 className="card-title font-title self-center text-2xl">Inicia sesión</h2>
                                     <p className="text-center ">No esperes para reservar, ingresa tus credenciales y comienza a pedir.</p>
                                     <div className="card-actions justify-center">
-                                        <button className="bg-secondary hover:bg-base-100 transition-all rounded-full p-2 text-white mt-2 px-8 font-bold">Ingresar</button>
+                                        <Link to="/auth/login" className="bg-secondary hover:bg-base-100 transition-all rounded-full p-2 text-white mt-2 px-8 font-bold">Ingresar</Link>
                                     </div>
                                 </div>
                             </div>
                         </div>
-                    <div className="divider md:divider-horizontal before:bg-primary after:bg-primary text-white">O</div>
+                        <div className="divider md:divider-horizontal before:bg-primary after:bg-primary text-white">O</div>
                         <div id="SignUpCard">
                             <div className="card lg:card-side h-full !bg-[#2c2c2c] shadow-xl min-w-[300px] max-w-[300px] lg:min-w-[425px] lg:max-w-[425px]">
                                 <div className="card-body">
                                     <h2 className="card-title font-title self-center text-2xl">Regístrate</h2>
                                     <p className="text-center self-center inline-block py-3">Reserva, disfruta, repite. Únete a D'Lish hoy mismo.</p>
                                     <div className="card-actions justify-center">
-                                        <button className="bg-primary hover:bg-base-100 transition-all rounded-full p-2 text-white mt-2 px-8 font-bold">Ingresar</button>
+                                        <Link to="auth/register" className="bg-info hover:bg-base-100 transition-all rounded-full p-2 text-white mt-2 px-8 font-bold">Ingresar</Link>
                                     </div>
                                 </div>
                                 <figure><img src={SignUpCardImg} alt="SignUpCardImg" className="w-full h-40 object-cover object-[100%_60%] lg:w-80 lg:h-[14.5rem]" /></figure>
