a08768a9cdc0906451e2aa02bc0a1f04ab4ed9ef
Vinic`-` <viictorgarcia01@gmail.com>
2023-08-17 21:04:18 -0600

    props en el admin

---
+++ react-d'lish/src/components/Admin/UsersProps.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import ExamplePfp from "../../assets/img/example-pfp.jpg";
+
+function UsersProps(props) {
+  return (
+    <div>
+                      <div className="overflow-y-auto max-height: 24rem" >
+                  <ul className="p-6 space-y-6">
+                    <li className="flex items-center">
+                      <div className="h-10 w-10 mr-3 bg-gray-100 rounded-full overflow-hidden">
+                        <img src={ExamplePfp} alt="Annette Watson profile picture" />
+                      </div>
+                      <span className="text-yellow-50">{props.name}</span>
+                    </li>
+                  </ul>
+                </div>
+    </div>
+  )
+}
+
+export default UsersProps
