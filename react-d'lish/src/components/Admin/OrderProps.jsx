138a99de98f47aaabf02a9eaae5f7a7974263823
Vinic`-` <viictorgarcia01@gmail.com>
2023-08-26 13:50:30 -0600

    solo border radius

--- react-d'lish/src/components/Admin/OrderProps.jsx
+++ react-d'lish/src/components/Admin/OrderProps.jsx
@@ -8,7 +8,7 @@ function OrderProps(props) {
 <section className='mt-4'>
         <div className="flex flex-col sm:w-[97%] w-[90%] lg:flex-row sm:p-5 ">
 
-         <div className="grid md:flex h-56 p-5 flex-grow  bg-secu place-items-center  md:w-11 lg:w-11 xl:w-11 ">
+         <div className="grid md:flex h-56 p-5 flex-grow  bg-secu place-items-center  md:w-11 lg:w-11 xl:w-11 rounded-t-lg">
          <section className="font-medium text-center">
                <img src={Pupusas} alt="" className='w-40 aspect-[3/2] rounded-lg object-cover object-top' />
                <h1 className=' font-extrabold text-primary text-lg'>{platillo}</h1></section>
@@ -32,7 +32,7 @@ function OrderProps(props) {
        </div>
             {/* Botones */}
         <div className="flex flex-col sm:w-[97%] w-[90%] lg:flex-row sm:p-5  h-32 md:h-56 ">
-          <div className="flex flex-row flex-grow gap-3 justify-center bg-secu md:bg-transparent place-items-center md:h-42  h-40   "> {/*  estaba en grid*/}
+          <div className="flex flex-row flex-grow gap-3 justify-center bg-secu md:bg-transparent place-items-center md:h-42  h-40 rounded-b-lg  "> {/*  estaba en grid*/}
           <td className="font-medium text-center text-primary "><button className="btn btn-outline btn-success">Aceptar</button></td>
            <td className="font-medium text-center text-primary "><button className="btn btn-outline btn-error">Denegar</button></td>
         </div>
