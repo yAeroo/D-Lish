6ad5810d45c63451822a6c39f1eefd07d5df4798
FloppaCoder <87096987+FloppaCoder@users.noreply.github.com>
2023-08-27 01:06:25 -0600

    NavCafetin ahora con Scroll Effect

--- react-d'lish/src/components/Nav/NavCafetin.jsx
+++ react-d'lish/src/components/Nav/NavCafetin.jsx
@@ -1,27 +1,45 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react';
 import { IoChevronBack } from "react-icons/io5";
 import { FaUserCircle } from "react-icons/fa";
 import { FaRegHeart } from "react-icons/fa";
 import { Link } from 'react-router-dom';
 
 const NavCafetin = () => {
+  const [showMenu, setShowMenu] = useState(false);
+  const [scrolling, setScrolling] = useState(false);
+
+  const handleScroll = () => {
+    if (window.scrollY > 0) {
+      setScrolling(true);
+    } else {
+      setScrolling(false);
+    }
+  };
+
+  useEffect(() => {
+    window.addEventListener('scroll', handleScroll);
+
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, []);
+
   return (
-    <>
-        <nav className='flex justify-between rounded-2xl drop-shadow-md px-9 py-5 list-none'>
+    <section className={`py-0 px-4 md:py-0 md:px-0 ${scrolling ? 'bg-transparent' : ''}`}>
+      <nav className={`flex justify-between rounded-2xl md:rounded-2xl md:mx-9 md:my-3 px-8 py-4 mt-3 md:px-[3rem] md:py-5 list-none ${scrolling ? 'bg-neutral' : 'bg-transparent md:bg-transparent'}`}>
         <Link onClick={() => window.history.back()}> 
-            <div className='flex items-center'><IoChevronBack className="w-10 h-12 mr-5 font-bold"/>
-
+          <div className='flex items-center'>
+            <IoChevronBack className="w-10 h-12 mr-5 font-bold"/>
             <h1 className="title text-[1.5rem] hidden md:block">Regresar</h1>
-            <h1 className="title hidden">Regresar</h1> </div>
-            </Link>
+            <h1 className="title hidden">Regresar</h1>
+          </div>
+        </Link>
             
-            <FaUserCircle className="w-12 h-12 ml-auto"/>
+        <FaUserCircle className="w-12 h-12 ml-auto"/>
             
-        </nav>
-
-      
-    </>
-  )
+      </nav>
+    </section>
+  );
 }
 
-export default NavCafetin
+export default NavCafetin;
