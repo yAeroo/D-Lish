74ac74f8497d258ac2c1685fe3d59c49cdc1a5fd
ELMEROMERORACPLUS12 <oscararcepineda@gmail.com>
2023-08-28 15:59:29 -0600

    momentos antes de que la cague total y completamente :v

--- react-d'lish/src/views/Admin/ProductAdmin.jsx
+++ react-d'lish/src/views/Admin/ProductAdmin.jsx
@@ -1,49 +1,62 @@
-import React from 'react'
-import {AiFillDelete} from "react-icons/ai"
-import ProductRow from '../../components/Admin/ProductRow';
-import ProductModal from '../../components/Admin/ProductModal';
+import React from "react";
+import { AiFillDelete } from "react-icons/ai";
+import ProductRow from "../../components/Admin/ProductRow";
+import ProductModal from "../../components/Admin/ProductModal";
 
 // Css
-import '../../css/productsTable.css'; 
-import DeleteModal from '../../components/Admin/ProductDeleteModal';
-
+import "../../css/productsTable.css";
+import DeleteModal from "../../components/Admin/ProductDeleteModal";
 
 function ProductAdmin() {
-	const almuerzodesayuno = [
-	  { id: 1, name: 'Producto 1', img: 'image1.jpg', price: 10.99 },
-	  { id: 2, name: 'Producto 2', img: 'image2.jpg', price: 19.99 },
-	  { id: 3, name: 'Producto 3', img: 'image3.jpg', price: 12.99 },
-	  // ... aquí es de agregar mas productos ...
-	];
-  
-	return (
+  const main_dish = [
+    { id: 1, name: "Producto 1", img: "image1.jpg", price: 10.99 },
+    { id: 2, name: "Producto 2", img: "image2.jpg", price: 19.99 },
+    { id: 3, name: "Producto 3", img: "image3.jpg", price: 12.99 },
+    // ... aquí es de agregar mas productos ...
+  ];
 
-        <div className='lg:ml-[10rem] mb-10 sm:ml-[5rem] ml-[1rem] h-[100%] animate-fade animate-duration-500'>
-            
+  const side_dish1 = [];
 
-			{/* ------- Open the modal using ID.showModal() method ----------*/}
-			<button className="btn btn-primary mt-[48px] sm:ml-[36px] sm:mt-[150px]" onClick={()=>window.product_modal_1.showModal()}>Agregar producto</button>
-				<ProductModal ModalId="product_modal_1" ModalTypebtn="Subir producto" ModalType="Agrega cualquier tipo de producto"/>
-			
-			{/* ------------- FIN DEL MODAL ------------ */}
+  const side_dish2 = [];
 
-			<h3 className='my-7 text-center text-terc font-bold text-2xl'> Almuerzos/Desayunos </h3>
+  const accompaniement = [];
 
-<div className="flex items-center w-[95%] justify-center">
+  const drink = [];
 
+  return (
+    <div className="lg:ml-[10rem] mb-10 sm:ml-[2rem] md:ml-[5rem] ml-[1rem] h-[100%] animate-fade animate-duration-500">
+      {/* ------- Open the modal using ID.showModal() method ----------*/}
+      <button
+        className="btn btn-primary mt-[48px] sm:ml-[36px] sm:mt-[150px]"
+        onClick={() => window.product_modal_1.showModal()}
+      >
+        Agregar producto
+      </button>
+      <ProductModal
+        ModalId="product_modal_1"
+        ModalTypebtn="Subir producto"
+        ModalType="Agrega cualquier tipo de producto"
+      />
 
+      {/* ------------- FIN DEL MODAL ------------ */}
 
+      <h3 className="my-7 text-center text-terc font-bold text-2xl">
+        {" "}
+        Almuerzos/Desayunos{" "}
+      </h3>
 
-	<div class="container">
-		<table class="w-full flex flex-row flex-no-wrap sm:bg-white rounded-lg overflow-hidden sm:shadow-lg my-5">
-			<thead class="text-white">
-				
-			{almuerzodesayuno.map((product) => (
-				// Encabezado de la tabla para cada producto
+      <div className="flex items-center w-[95%] justify-center">
+        <div className="container">
+          <table className="w-full flex flex-row flex-no-wrap sm:bg-white rounded-lg overflow-hidden sm:shadow-lg my-5">
+            <thead className="text-white">
+              {main_dish.map((main_dish) => (
+                // Encabezado de la tabla para cada producto
                 <tr className="bg-terc flex flex-col flex-no wrap sm:table-row rounded-l-lg sm:rounded-none mb-2 sm:mb-0">
                   <th className="p-3 text-left">Id</th>
                   <th className="p-3 text-left">Nombre</th>
-                  <th className="p-3 text-left h-[233px] sm:h-[49px]">Imagen</th>
+                  <th className="p-3 text-left h-[242px] sm:h-[49px]">
+                    Imagen
+                  </th>
                   <th className="p-3 text-left" width="110px">
                     Precio
                   </th>
@@ -52,39 +65,44 @@ function ProductAdmin() {
                   </th>
                 </tr>
               ))}
-
-
-				{/* <tr class="bg-teal-400 flex flex-col flex-no wrap sm:table-row rounded-l-lg sm:rounded-none mb-2 sm:mb-0">
-					<th class="p-3 text-left">Name</th>
-					<th class="p-3 text-left">Email</th>
-					<th class="p-3 text-left" width="110px">Actions</th>
-				</tr> */}
-			</thead>
-			<tbody class="flex-1 sm:flex-none text-[#414141]">
-				
-			{almuerzodesayuno.map((product) => (
+            </thead>
+            <tbody className="flex-1 sm:flex-none text-[#414141]">
+              {main_dish.map((main_dish) => (
                 <ProductRow
-                  key={product.id}
-                  ProductNum={product.id}
-                  ProductItem={product.name}
-                  ProductIMG={product.img}
-                  ProductPrice={product.price}
+                  key={main_dish.id}
+                  ProductNum={main_dish.id}
+                  ProductItem={main_dish.name}
+                  ProductIMG={main_dish.img}
+                  ProductPrice={main_dish.price}
                 />
-
-				
               ))}
 
-			<ProductModal ModalId="product_modal_2" ModalTypebtn="Guardar cambios" ModalType="Actualiza un producto ya existente"/>
-			<DeleteModal/>
-			</tbody>
-		</table>
-	</div>
-</div>
+              <ProductModal
+                ModalId="product_modal_2"
+                ModalTypebtn="Guardar cambios"
+                ModalType="Actualiza un producto ya existente"
+              />
+              <DeleteModal />
+            </tbody>
+          </table>
+        </div>
+      </div>
 
-		
-	</div >
+      <div className="hidden">
+        <h3 className="my-7 text-center text-terc font-bold text-2xl ">
+          {" "}
+          Acompañamientos{" "}
+        </h3>
+      </div>
 
-    )
-    }
+      <div className="hidden">
+        <h3 className="my-7 text-center text-terc font-bold text-2xl hidden">
+          {" "}
+          Bebidas{" "}
+        </h3>
+      </div>
+    </div>
+  );
+}
 
-export default ProductAdmin
+export default ProductAdmin;
