3e10e57879b2e45defa66b0f5fd558c9942fc0a6
FloppaCoder <87096987+FloppaCoder@users.noreply.github.com>
2023-08-29 22:22:07 -0600

    Clases active funcionales | MobileNav

--- react-d'lish/src/components/Nav/NavMobileIndex.jsx
+++ react-d'lish/src/components/Nav/NavMobileIndex.jsx
@@ -4,23 +4,23 @@ import { FaUserCircle } from "react-icons/fa";
 import { FaHome } from "react-icons/fa";
 import { FaSearch } from "react-icons/fa";
 //Link
-import { Link } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 
 export default function NavMobileIndex() {
   return (
     <div className="btm-nav md:hidden text-primary z-10 bg-base-300">
-      <Link to="../search" className="flex items-center gap-1">
+      <NavLink to="/search" activeClassName="active" className="flex items-center gap-1 bg-base-300">
         <FaSearch className="h-5 w-5 mx-auto" />
         <span className="btm-nav-label text-sm">Busqueda</span>
-      </Link>
-      <Link to="/" className="active bg-base-300">
+      </NavLink>
+      <NavLink to="/" activeClassName="active" className="bg-base-300">
         <FaHome className="h-5 w-5 mx-auto" />
         <span className="btm-nav-label text-sm">Inicio</span>
-      </Link>
-      <Link to="../profile" className="flex items-center gap-1">
+      </NavLink>
+      <NavLink to="/profile" activeClassName="active" className="flex items-center gap-1 bg-base-300">
         <FaUserCircle className="h-5 w-5 ml-[0.1rem]" />
         <span className="btm-nav-label text-sm">Perfil</span>
-      </Link>
+      </NavLink>
     </div>
   );
 }
