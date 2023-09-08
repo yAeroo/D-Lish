a8a6e882efa42ae91b5afff07cb24c0dc711c925
FloppaCoder <87096987+FloppaCoder@users.noreply.github.com>
2023-09-08 12:49:55 -0600

    Avance TTraducciones

--- react-d'lish/src/views/Almuerzos.jsx
+++ react-d'lish/src/views/Almuerzos.jsx
@@ -1,4 +1,5 @@
 import React, { useEffect } from "react";
+import { useTranslation } from "react-i18next";
 
 //Componentes
 import NavCafetin from "../components/Nav/NavCafetin";
@@ -16,6 +17,8 @@ import PolloAsado from "/src/assets/index/polloAsado.jpg";
 
 
 export default function Desayunos() {
+  const { t } = useTranslation();
+
   useEffect(() => {
     // Cuando el componente se monta, ajusta el scroll al principio de la página
     window.scrollTo(0, 0);
@@ -32,7 +35,7 @@ export default function Desayunos() {
             <div className="flex w-full h-[4rem] z-0 md:mb-[4rem]"></div>
             <div className="flex justify-between lg:items-center bg-[#5a2d96] xxsm:h-[11rem] md:h-[13rem] lg:h-[17rem] mt-[-1rem] md:px-7 md:pt-1 rounded-t-2xl drop-shadow-md animate-fade-up animate-ease-in-out animate-once animate-duration-[500ms] relative">
               <h1 className="text-4xl md:text-6xl font-bold xxsm:mt-[2rem] md:mt-[3rem] lg:mt-[0rem] pb-[1rem] xxsm:ml-7 md:ml-6 md:px-1 text-white">
-                Almuerzos
+                {t("almuerzo.lunch-title")}
               </h1>
               <div className="flex flex-col items-end absolute right-0 xxsm:top-[3.5rem] md:top-[1rem] lg:bottom-auto lg:top-[0rem] lg:right-[0rem]">
                 <img
@@ -44,7 +47,7 @@ export default function Desayunos() {
           </section>
           <section className="bg-base-100 text-white mt-[-1rem] rounded-t-2xl items-center justify-between mx-0 xl:mx-[0rem] animate-fade-up animate-ease-in-out animate-once animate-duration-[500ms]">
             <h2 className="text-3xl md:text-5xl font-bold drop-shadow-2xl pt-12 px-8 text-center">
-              Popular entre Estudiantes
+            {t("almuerzo.popular-title")}
             </h2>
             <div className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-2 xl:mx-[6rem] gap-4 md:gap-8 xl:gap-12 px-8 pt-8 pb-[10rem]">
               <FoodCard
--- react-d'lish/src/views/Otros.jsx
+++ react-d'lish/src/views/Otros.jsx
@@ -1,4 +1,5 @@
 import React, { useEffect } from "react";
+import { useTranslation } from "react-i18next";
 
 //Componentes
 import NavCafetin from "../components/Nav/NavCafetin";
@@ -13,6 +14,9 @@ import JugosNaturales from "/src/assets/index/jugosNaturales.jpg";
 import Licuado from "/src/assets/index/licuado.jpg";
 
 export default function Desayunos() {
+
+  const { t } = useTranslation();
+
   useEffect(() => {
     // Cuando el componente se monta, ajusta el scroll al principio de la página
     window.scrollTo(0, 0);
@@ -29,7 +33,7 @@ export default function Desayunos() {
             <div className="fflex w-full h-[4rem] z-0 md:mb-[4rem]"></div>
             <div className="flex justify-between lg:items-center bg-orange-500 xxsm:h-[11rem] md:h-[13rem] lg:h-[17rem] mt-[-1rem] md:px-7 md:pt-1 rounded-t-2xl drop-shadow-md animate-fade-up animate-ease-in-out animate-once animate-duration-[500ms] relative">
               <h1 className="text-4xl md:text-6xl font-bold xxsm:mt-[2rem] md:mt-[3rem] lg:mt-[0rem] pb-[1rem] xxsm:ml-7 md:ml-6 md:px-1 text-white">
-                Otros
+              {t("otros.other-title")}
               </h1>
               <div className="flex flex-col items-end absolute right-0 xxsm:top-[3.5rem] md:top-[1rem] lg:bottom-auto lg:top-[0rem] lg:right-[0rem]">
                 <img
@@ -41,7 +45,7 @@ export default function Desayunos() {
           </section>
           <section className="bg-base-100 text-white mt-[-1rem] rounded-t-2xl items-center justify-between mx-0 xl:mx-[0rem] animate-fade-up animate-ease-in-out animate-once animate-duration-[500ms]">
             <h2 className="text-3xl md:text-5xl font-bold drop-shadow-2xl pt-12 px-8 text-center">
-              Popular entre Estudiantes
+            {t("otros.popular-title")}
             </h2>
             <div className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-2 xl:mx-[6rem] gap-4 md:gap-8 xl:gap-12 px-8 pt-8 pb-[10rem]">
               <FoodCard
